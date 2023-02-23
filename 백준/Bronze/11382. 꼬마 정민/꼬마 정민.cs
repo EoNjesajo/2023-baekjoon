e2fc09a1c0d53be4d5c9074f7a3bd34c94fb87e3
WonGyeom <90492809+EoNjesajo@users.noreply.github.com>
2023-02-23 11:36:13 +0900

    [Bronze V] Title: 꼬마 정민, Time: 60 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string[] input = Console.ReadLine().Split();
+    long a = long.Parse(input[0]);
+    long b = long.Parse(input[1]);
+    long c = long.Parse(input[2]);
+    Console.WriteLine(a+b+c);
+  }
+}
+
